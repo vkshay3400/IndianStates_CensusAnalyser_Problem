9ba28e20c6803326145483ac1c7afeea57a46aba
vkshay3400 <akshayp2208@gmail.com>
2020-03-17 17:50:53 +0530

    Added: Welcome message

---
+++ src/main/java/com/bridgelabz/IndianStatesCensusAnalyser.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class IndianStatesCensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian States Census Analyser");
+    }
+}

3b6a9946f3d4f41aa643a51caec3fe3f36ad622a
vkshay3400 <akshayp2208@gmail.com>
2020-03-20 16:32:33 +0530

    Refactor: As DRY principle was avoided to get count now changed using genrics

--- src/main/java/com/bridgelabz/censusanalyserproject/IndianStatesAnalyser.java
+++ src/main/java/com/bridgelabz/censusanalyserproject/IndianStatesAnalyser.java
@@ -11,60 +11,22 @@ import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.regex.Pattern;
+import java.util.stream.StreamSupport;
 
 public class IndianStatesAnalyser {
     //FOR CSV FILE
     private static final String CSV_FILE_PATTERN = "^[a-zA-Z0-9./_@]*[.]+[c][s][v]$";
 
     //METHOD FOR STATE CENSUS
-    public int loadIndianCensusData(String csvFilePath) throws MyExceptions, IOException {
-        int count = 0;
-
-        String extension = getFileExtension(csvFilePath);
-        if (!Pattern.matches(CSV_FILE_PATTERN, extension))
-            throw new MyExceptions(MyExceptions.Exception.PATH_NOT_FOUND, "No such a type");
-
-        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
-            Iterator<IndianCensusData> censusCSVIterator = this.getCsvFileIterator(reader, IndianCensusData.class);
-            while (censusCSVIterator.hasNext()) {
-                System.out.print(count++ + " ");
-                IndianCensusData censusCSV = censusCSVIterator.next();
-                System.out.print("state: " + censusCSV.getState() + ", ");
-                System.out.print("population: " + censusCSV.getPopulation() + ", ");
-                System.out.print("area: " + censusCSV.getAreaInSqKm() + ", ");
-                System.out.print("density: " + censusCSV.getDensityPerSqKm() + ", ");
-                System.out.println();
-            }
-        } catch (NoSuchFileException e) {
-            throw new MyExceptions(MyExceptions.Exception.FILE_NOT_FOUND, "File not found");
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (RuntimeException e) {
-            throw new MyExceptions(MyExceptions.Exception.WRONG_DELIMITER_OR_HEADER, "Wrong Delimiter or Header");
-        }
-        return count;
-    }
-
-    //METHOD FOR STATE CODE
-    public int loadIndianStateCodeData(String csvFilePath) throws MyExceptions {
+    public int loadIndianCensusData(String csvFilePath) throws MyExceptions {
         //LOCAL VARIABLE
         int recordCount = 0;
         String extension = getFileExtension(csvFilePath);
         if (!Pattern.matches(CSV_FILE_PATTERN, extension))
             throw new MyExceptions(MyExceptions.Exception.PATH_NOT_FOUND, "No such a type");
-
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
-            Iterator<IndianStateCode> statesCSVIterator = this.getCsvFileIterator(reader, IndianStateCode.class);
-            while (statesCSVIterator.hasNext()) {
-                IndianStateCode censusCSV = statesCSVIterator.next();
-                ++recordCount;
-                System.out.print("SrNo: " + censusCSV.getSrNo() + ", ");
-                System.out.print("state: " + censusCSV.getState() + ", ");
-                System.out.print("Name: " + censusCSV.getName() + ", ");
-                System.out.print("TIN: " + censusCSV.getTIN() + ", ");
-                System.out.print("StateCode: " + censusCSV.getStateCode() + ", ");
-                System.out.println();
-            }
+            Iterator<IndianCensusData> censusCSVIterator = this.getCsvFileIterator(reader, IndianCensusData.class);
+            return this.getCount(censusCSVIterator);
         } catch (NoSuchFileException e) {
             throw new MyExceptions(MyExceptions.Exception.FILE_NOT_FOUND, "File not found");
         } catch (IOException e) {
@@ -75,26 +37,53 @@ public class IndianStatesAnalyser {
         return recordCount;
     }
 
-    //FOR FILE EXTENSION
-    private static String getFileExtension(String file) {
-        String format = null;
-        try {
-            if (file != null) {
-                format = file.substring(file.lastIndexOf("."));
-            }
-        } catch (Exception e) {
-            format = "";
+    //METHOD FOR STATE CODE
+    public int loadIndianStateCodeData(String csvFilePath) throws MyExceptions {
+        //LOCAL VARIABLE
+        int recordCount = 0;
+        String extension = getFileExtension(csvFilePath);
+        if (!Pattern.matches(CSV_FILE_PATTERN, extension))
+            throw new MyExceptions(MyExceptions.Exception.PATH_NOT_FOUND, "No such a type");
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
+            Iterator<IndianStateCode> codeCSVIterator = this.getCsvFileIterator(reader, IndianStateCode.class);
+            return this.getCount(codeCSVIterator);
+        } catch (NoSuchFileException e) {
+            throw new MyExceptions(MyExceptions.Exception.FILE_NOT_FOUND, "File not found");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (RuntimeException e) {
+            throw new MyExceptions(MyExceptions.Exception.WRONG_DELIMITER_OR_HEADER, "Wrong Delimiter or Header");
         }
-        return format;
+        return recordCount;
     }
 
-    //METHOD FOR CSV ITERATOR
-    private <E> Iterator<E> getCsvFileIterator(Reader reader, Class<E> csvClass) {
-        CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder(reader);
+    //METHOD TO GET EXTENSION OF CSV FILE
+    private static String getFileExtension(String file) {
+        String extension = "";
+        try {
+            if (file != null) {
+                extension = file.substring(file.lastIndexOf("."));
+            }
+        } catch (Exception e) {
+            extension = "";
+        }
+        return extension;
+    }
+
+    //GENERIC METHOD FOR CSV ITERATOR
+    private <T> Iterator<T> getCsvFileIterator(Reader reader, Class<T> csvClass) {
+        CsvToBeanBuilder<T> csvToBeanBuilder = new CsvToBeanBuilder(reader);
         csvToBeanBuilder.withType(csvClass);
         csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-        CsvToBean<E> csvToBean = csvToBeanBuilder.build();
+        CsvToBean<T> csvToBean = csvToBeanBuilder.build();
         return csvToBean.iterator();
+    }
+
+    //GENERIC METHOD TO GET COUNT
+    private <E> int getCount(Iterator<E> iterator) {
+        Iterable<E> iterable = () -> iterator;
+        int recordCount = (int) StreamSupport.stream(iterable.spliterator(), false).count();
+        return recordCount;
     }
 
     //MAIN METHOD

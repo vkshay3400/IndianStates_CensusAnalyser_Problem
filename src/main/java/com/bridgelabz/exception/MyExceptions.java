32dc557c62acd80c531829c4294277e4774b7bb0
vkshay3400 <akshayp2208@gmail.com>
2020-03-23 16:36:38 +0530

    Refactor: Changed method name

--- src/main/java/com/bridgelabz/exception/MyExceptions.java
+++ src/main/java/com/bridgelabz/exception/MyExceptions.java
@@ -1,13 +1,13 @@
 package com.bridgelabz.exception;
 
 public class MyExceptions extends Exception {
-    public enum Exception {
+    public enum Exception_Type {
         FILE_NOT_FOUND, PATH_NOT_FOUND, WRONG_DELIMITER_OR_HEADER;
     }
 
-    public Exception type;
+    public Exception_Type type;
 
-    public MyExceptions(Exception type, String message) {
+    public MyExceptions(Exception_Type type, String message) {
         super(message);
         this.type = type;
     }

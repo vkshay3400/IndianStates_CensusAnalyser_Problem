cd259de282b1ef84f706140533b126dc31300a8c
vkshay3400 <akshayp2208@gmail.com>
2020-03-23 16:43:49 +0530

    Refactor: To throw CsvBuilder exception

--- src/main/java/com/bridgelabz/censusanalyserproject/CsvBuilder.java
+++ src/main/java/com/bridgelabz/censusanalyserproject/CsvBuilder.java
@@ -1,18 +1,23 @@
 package com.bridgelabz.censusanalyserproject;
 
+import com.bridgelabz.exception.CsvBuilderException;
 import com.bridgelabz.exception.MyExceptions;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+
 import java.io.Reader;
 import java.util.Iterator;
 
-public class CsvBuilder implements IcsvBuilder  {
-    //GENERIC METHOD TO GET CSV ITERATOR
-    public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws MyExceptions {
-        CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder(reader);
-        csvToBeanBuilder.withType(csvClass);
-        csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-        CsvToBean<E> csvToBean = csvToBeanBuilder.build();
-        return csvToBean.iterator();
+public class CsvBuilder implements IcsvBuilder {
+    public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CsvBuilderException {
+        try {
+            CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder(reader);
+            csvToBeanBuilder.withType(csvClass);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<E> csvToBean = csvToBeanBuilder.build();
+            return csvToBean.iterator();
+        } catch (IllegalStateException e) {
+            throw new CsvBuilderException(MyExceptions.Exception_Type.FILE_NOT_FOUND, "Wrong file");
+        }
     }
 }
--- src/main/java/com/bridgelabz/censusanalyserproject/IcsvBuilder.java
+++ src/main/java/com/bridgelabz/censusanalyserproject/IcsvBuilder.java
@@ -1,10 +1,10 @@
 package com.bridgelabz.censusanalyserproject;
 
-import com.bridgelabz.exception.MyExceptions;
+import com.bridgelabz.exception.CsvBuilderException;
 
 import java.io.Reader;
 import java.util.Iterator;
 
 public interface IcsvBuilder {
-    public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws MyExceptions;
+    public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CsvBuilderException;
 }

fb15e0550b743d437c5e7160fcc258d350280e04
vkshay3400 <akshayp2208@gmail.com>
2020-03-24 16:10:22 +0530

    Added: Method to sort

--- src/main/java/com/bridgelabz/censusanalyserproject/IndianStatesAnalyser.java
+++ src/main/java/com/bridgelabz/censusanalyserproject/IndianStatesAnalyser.java
@@ -2,12 +2,14 @@ package com.bridgelabz.censusanalyserproject;
 
 import com.bridgelabz.exception.CsvBuilderException;
 import com.bridgelabz.exception.MyExceptions;
+import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -17,6 +19,9 @@ public class IndianStatesAnalyser {
     //FOR CSV FILE
     private static final String PATTERN_FOR_CSV_FILE = "^[a-zA-Z0-9./_@]*[.]+[c][s][v]$";
 
+    //FOR LIST
+    List<IndianCensusData> csvFileList = null;
+
     //METHOD FOR STATE CENSUS
     public int loadIndianCensusData(String csvFilePath) throws MyExceptions {
         String extension = getFileExtension(csvFilePath);
@@ -24,7 +29,7 @@ public class IndianStatesAnalyser {
             throw new MyExceptions(MyExceptions.Exception_Type.PATH_NOT_FOUND, "No such a path");
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
             IcsvBuilder csvBuilder = CsvBuilderFactory.createCsvBuilder();
-            List<IndianCensusData> csvFileList = csvBuilder.getCSVFileList(reader, IndianCensusData.class);
+            csvFileList = csvBuilder.getCSVFileList(reader, IndianCensusData.class);
             return csvFileList.size();
         } catch (CsvBuilderException e) {
             e.printStackTrace();
@@ -77,6 +82,31 @@ public class IndianStatesAnalyser {
         Iterable<E> iterable = () -> iterator;
         int recordCount = (int) StreamSupport.stream(iterable.spliterator(), false).count();
         return recordCount;
+    }
+
+    //METHOD FOR COMPARATOR
+    public String getSortedCensusStateData(String csvFilePath) throws MyExceptions {
+        loadIndianCensusData(csvFilePath);
+        if (csvFileList == null || csvFileList.size() == 0) {
+            throw new MyExceptions(MyExceptions.Exception_Type.NO_SUCH_CENSUS_DATA, "Census Data not found");
+        }
+        csvFileList.sort(Comparator.comparing(e -> e.getState()));
+        String toJson = new Gson().toJson(csvFileList);
+        return toJson;
+    }
+
+    //METHOD TO SORT
+    private void sort(List<IndianCensusData> csvFileList, Comparator<IndianCensusData> censusComparator) {
+        for (int index1 = 0; index1 < csvFileList.size(); index1++) {
+            for (int index2 = 0; index2 < csvFileList.size() - index1 - 1; index2++) {
+                IndianCensusData census1 = csvFileList.get(index2);
+                IndianCensusData census2 = csvFileList.get(index2 + 1);
+                if (censusComparator.compare(census1, census2) > 0) {
+                    csvFileList.set(index2, census2);
+                    csvFileList.set(index2 + 1, census1);
+                }
+            }
+        }
     }
 
     //MAIN METHOD

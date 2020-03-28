9eff13ceb5910560236e66c78322db0dcd4306ac
vkshay3400 <akshayp2208@gmail.com>
2020-03-28 16:07:26 +0530

    Added: US census data

--- src/main/java/com/bridgelabz/censusanalyserproject/USCensusData.java
+++ src/main/java/com/bridgelabz/censusanalyserproject/USCensusData.java
@@ -1,4 +1,36 @@
 package com.bridgelabz.censusanalyserproject;
 
+import com.opencsv.bean.CsvBindByName;
+
 public class USCensusData {
+    @CsvBindByName(column = " State Id", required = true)
+    private String stateId;
+    @CsvBindByName(column = "State", required = true)
+    private String state;
+    @CsvBindByName(column = "Population", required = true)
+    private int population;
+    @CsvBindByName(column = "Total area", required = true)
+    private double totalArea;
+    @CsvBindByName(column = "Population Density", required = true)
+    private double densityPerSqKm;
+
+    public String getStateId() {
+        return stateId;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public double getTotalArea() {
+        return totalArea;
+    }
+
+    public double getDensityPerSqKm() {
+        return densityPerSqKm;
+    }
 }

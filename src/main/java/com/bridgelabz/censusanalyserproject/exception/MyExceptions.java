b63209eaa0d5ed4564e24bb9dfed7d4e8c6d55c2
vkshay3400 <akshayp2208@gmail.com>
2020-03-30 15:11:05 +0530

    Added: Enum no such country

--- src/main/java/com/bridgelabz/censusanalyserproject/exception/MyExceptions.java
+++ src/main/java/com/bridgelabz/censusanalyserproject/exception/MyExceptions.java
@@ -2,7 +2,7 @@ package com.bridgelabz.censusanalyserproject.exception;
 
 public class MyExceptions extends Exception {
     public enum Exception_Type {
-        FILE_NOT_FOUND, PATH_NOT_FOUND, WRONG_DELIMITER_OR_HEADER,NO_SUCH_CENSUS_DATA;
+        FILE_NOT_FOUND, PATH_NOT_FOUND, WRONG_DELIMITER_OR_HEADER,NO_SUCH_CENSUS_DATA,NO_SUCH_COUNTRY;
     }
 
     public Exception_Type type;
